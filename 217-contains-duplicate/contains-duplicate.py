607de8a165b458097e0878c50cafc79c32590337
DataWhizJyoti <155168612+DataWhizJyoti@users.noreply.github.com>
2024-01-22 15:06:15 +0530

    Time: 443 ms (36.33%) | Memory: 27.4 MB (79.07%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -4,13 +4,35 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
+        """ Brute Force 
+            Time Complexity-O(N^2)
+            Space Complexity-O(1)
+        
+        for i in range(0, len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i]==nums[j]:
+                    return True
+        return False
+
+        Better Solution
+        Time Complexity-O(NlogN)
+        Space Complexity-O(1)
+        
         nums.sort()
         for i in range(0,len(nums)-1):
             if nums[i]==nums[i+1]:
                 return True
         return False
 
-        """Better Solution
-        Time Complexity-O(NlogN)
-        Space Complexity-O(1)"""
+        Optimal Solution
+        Time Complexity-O(N)
+        Space Complexity-O(N)"""
+
+        hashset=set()
+        for i in range(0,len(nums)):
+            if nums[i] in hashset:
+                return True
+            else:
+                hashset.add(nums[i])
+        return False
         
